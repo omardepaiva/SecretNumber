c66675695c3e5cccd6f688f1c8e390c5e4a7f20b
Omar de Paiva <omar.paiva@gmail.com>
2023-01-25 09:34:44 -0300

    reconhecimento de voz

--- app/sortearNumber.js
+++ app/sortearNumber.js
@@ -1,7 +1,16 @@
+const menorValor = 1;
+const maiorValor = 100;
+
 const numeroSecreto = gerarNumeroAleatorio();
 
 function gerarNumeroAleatorio() {
-  return parseInt(Math.random() * 100);
+  return parseInt(Math.random() * maiorValor + 1);
 }
 
 console.log(numeroSecreto);
+
+const elementoMenorValor = document.getElementById("menor-valor");
+elementoMenorValor.innerHTML = menorValor;
+
+const elementoMaiorValor = document.getElementById("maior-valor");
+elementoMaiorValor.innerHTML = maiorValor;
