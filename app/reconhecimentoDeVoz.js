77711f0ddc2328442b9745dd027f2d158b0f6300
Omar de Paiva <omar.paiva@gmail.com>
2023-01-25 13:31:15 -0300

    projeto final

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -1,9 +1,9 @@
 const elementoChute = document.getElementById("chute");
+
 window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
-
-recognition.lang = "pt-br";
+recognition.lang = "pt-Br";
 recognition.start();
 
 recognition.addEventListener("result", onSpeak);
@@ -11,12 +11,14 @@ recognition.addEventListener("result", onSpeak);
 function onSpeak(e) {
   chute = e.results[0][0].transcript;
   exibeChuteNaTela(chute);
+  verificaSeOChutePossuiUmValorValido(chute);
 }
 
 function exibeChuteNaTela(chute) {
   elementoChute.innerHTML = `
-      <div>Você disse:</div>
-      <span class="box">${chute}</span>
-     
-`;
+        <div>Você disse</div>
+        <span class="box">${chute}</span>
+     `;
 }
+
+recognition.addEventListener("end", () => recognition.start());
--- app/sortearNumber.js
+++ app/sortearNumber.js
@@ -1,13 +1,12 @@
 const menorValor = 1;
-const maiorValor = 100;
-
+const maiorValor = 1000;
 const numeroSecreto = gerarNumeroAleatorio();
 
 function gerarNumeroAleatorio() {
   return parseInt(Math.random() * maiorValor + 1);
 }
 
-console.log(numeroSecreto);
+console.log("Número Secreto:", numeroSecreto);
 
 const elementoMenorValor = document.getElementById("menor-valor");
 elementoMenorValor.innerHTML = menorValor;
